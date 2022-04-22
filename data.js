9184bb00aad361125e730e7e989bb2b3e4c341da
BrayanLopez <balg.lg55@gmail.com>
2022-04-22 11:51:34 -0500

    first commit

---
+++ data.js
@@ -0,0 +1,100 @@
+var data = [
+  {
+    word: "Aritmética",
+    prohibed: ["Números", "Operación", "Contar"],
+  },
+  {
+    word: "Álgebra",
+    prohibed: ["Ecuación", "Polinomio", "Inecuación"],
+  },
+  {
+    word: "Geometría",
+    prohibed: ["Figura", "Recta", "Circulo"],
+  },
+  {
+    word: "Trigonométria",
+    prohibed: ["Seno", "Coseno", "Tangente"],
+  },
+  {
+    word: "Probabilidad",
+    prohibed: ["Dado", "Posibilidad", "Aleatorio"],
+  },
+  {
+    word: "Estadística",
+    prohibed: ["Gráficas", "Datos", "Tendencia"],
+  },
+  {
+    word: "Función",
+    prohibed: ["Gráficas", "Relación", "Dependencia"],
+  },
+  {
+    word: "Ecuación",
+    prohibed: ["Lineal", "Raiz", "Igualdad"],
+  },
+  {
+    word: "Derivada",
+    prohibed: ["Cambio", "Diferencia", "Derivación"],
+  },
+  {
+    word: "Integral",
+    prohibed: ["Pan", "Derivada", "Cálculo"],
+  },
+  {
+    word: "Lógica",
+    prohibed: ["Inferencia", "Validez", "Prueba"],
+  },
+  {
+    word: "Conjunto",
+    prohibed: ["Agrupación", "Colección", "Venn"],
+  },
+  {
+    word: "Incognita",
+    prohibed: ["Desconocido", "X", "Problema"],
+  },
+  {
+    word: "Teorema",
+    prohibed: ["Demostrable", "Verdad", "Conclusión"],
+  },
+  {
+    word: "Axioma",
+    prohibed: ["Obvio", "Verdad", "Evidente"],
+  },
+  {
+    word: "Abstracción",
+    prohibed: ["Atributos", "Concreto", "Abstracto"],
+  },
+  {
+    word: "Demostración",
+    prohibed: ["Deducción", "Verdad", "Implicación"],
+  },
+  {
+    word: "Sistema de numeración",
+    prohibed: ["Binario", "Hexadecimal", "Octal"],
+  },
+  {
+    word: "Conjunto numérico",
+    prohibed: ["Reales", "Naturales", "Enteros"],
+  },
+  {
+    word: "Física",
+    prohibed: ["Natural", "Fenomenos", "Leyes"],
+  },
+  {
+    word: "Vector",
+    prohibed: ["Dirección", "Magnitud", "Sentido"],
+  },
+  {
+    word: "Cinemática",
+    prohibed: ["Movimiento", "Velocidad", "Aceleración"],
+  },
+  {
+    word: "Dinámica",
+    prohibed: ["Newton", "Trabajo", "Fuerza"],
+  },
+  {
+    word: "Óptica",
+    prohibed: ["Luz", "Ondulatorio", "Reflexión"],
+  },
+];
+
+console.log(data.length);
